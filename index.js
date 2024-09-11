ddf76d2576973696a13ff7ed08f03809474f02af
Doniyor <ddonierov68@gmail.com>
2024-09-11 16:44:55 +0500

    finished project

--- index.js
+++ index.js
@@ -1,41 +1,107 @@
-#! /usr/github_user_activity_cli/env node
-
-
-// token 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
+// Code #1
 function findingUser(username) {
-    const api =  `https://api.github.com/users/${username}/events`
+    const api = `https://api.github.com/users/${username}/events`
     fetch(api)
         .then(res => res.json())
         .then(json => {
-            const list = []
-            // const len = json.filter(item => item.type === "PushEvent").length
-            // console.log(len2)
             for (let i = 0; i < json.length; i++) {
                 const element = json[i];
-
-                if(element.type === "PushEvent") {
-                    // console.log(`Pushed ${element.payload.commits.length} commits to ${element.repo.name}`)
+                let action;
+                if (element.type === "PushEvent") {
+                    action = `Pushed ${element.payload.commits.length} commits to ${element.repo.name}`
+                } else if (element.type === "IssuesEvent") {
+                    action = `${element.payload.action.char(0).toUpperCase() + element.payload.action.slice(1)} an issues in ${element.repo.name}`
+                } else if(element.type === "ForkEvent") {
+                    action = `Forked ${element.repo.name}`
+                } else if (element.type === "WatchEvent") {
+                    action = `Starred ${element.repo.name}`
+                } else if (element.type === "CreateEvent") {
+                    action = `Created ${element.payload.ref_type} in ${element.repo.name}`
                 }
-
-                if(element.type === "PullRequestEvent") {
-                    let action = element.payload.action
-                    action = action[0].toUpperCase() + action.slice(1)
-                    if(element.payload.action === "closed") {
-                        // console.log(`${action} a pull in ${element.repo.name}`)
-                    }else{
-                        // console.log(`${action} a new pull in ${element.repo.name}`)
-                    }
+                else {
+                    action = `${element.type.replace("Event", "")} in ${element.repo.name}`
                 }
-                
-                list.push(element.type)
+                console.log(`- ${action}`)
             }
-            const newArray = [...new Set(list)]
-            console.log(newArray)
+        }).catch((err )=> {
+            console.log(err)
+            process.exit(1)
         })
 }
 
-if(typeof(process.argv[2]) === "string") {
+if (process.argv[2]) {
     findingUser(process.argv[2])
-}else{
+} else {
     console.log("please enter the username")
+    process.exit(1)
 }
+
+// Code #2
+// async function githubUserActivityApi(username) {
+//     const res = await fetch(`https://api.github.com/users/${username}/events`)
+//     if (!res.ok) {
+//         if (res.status === 404) {
+//             throw new Error("User not Found. Please check your username")
+//         } else {
+//             throw new Error(`Error fetching data: ${res.status}`)
+//         }
+//     }
+//     return res.json()
+// }
+
+
+// function displayActivity(events) {
+//     if (events.length === 0) {
+//         console.log("events is empty")
+//     }
+
+//     for (let i = 0; i < events.length; i++) {
+//         const element = events[i];
+//         let action;
+//         switch (element.type) {
+//             case "PushEvent":
+//                 const len = element.payload.commits.length
+//                 action = `Pushed ${len} commit(s) to ${element.repo.name}`
+//                 break;
+
+//             case "IssuesEvent":
+//                 const str = element.payload.action.charAt(0).toUpperCase() + element.payload.action.slice(1)
+//                 action = `${str} an issues in ${element.repo.name}`
+//                 break;
+
+//             case "WatchEvent":
+//                 action = `Starred ${element.repo.name}`
+//                 break;
+//             case "ForkEvent":
+//                 action = `Forked ${element.repo.name}`
+//                 break;
+//             case "CreateEvent":
+//                 action = `Created ${element.payload.ref_type} in ${element.repo.name}`
+//                 break;
+//             default:
+//                 action = `${element.type.replace("Event", "")} in ${element.repo.name}`
+//                 break;
+//         }
+
+//         console.log(`- ${action}`)
+//     }
+// }
+
+// const startApp = () => {
+//     const username = process.argv[2]
+//     if (!username) {
+//         console.log("Please enter the Github username")
+//         process.exit(1)
+//     }
+
+//     githubUserActivityApi(username)
+//         .then((events) => {
+//             displayActivity(events)
+//         }).catch((err) => {
+//             console.log(err.message)
+//             process.exit(1)
+//         })
+// }
+
+
+// startApp()
